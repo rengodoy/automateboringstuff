63ccd7f94e8233dac0fd60aad55a462506b57394
Renato Machado de Godoy <renato.godoy@ifmg.edu.br>
2021-08-14 11:15:14 -0300

    Dictionaries

--- lists.py
+++ lists.py
@@ -9,10 +9,14 @@ The slice has two indexes. The new list's items start at the first index and go 
 The len() function, concatenation, and replication work the same way on lists that they do with strings.
 You can convert a value into a list by passing it to the list() function.
 
+#------------------------------------------------------------------------------------------
+
 A for loop technically iterates over the values in a list.
 The range() function returns a list-like value, which can be passed to the list() function if you need an actual list value.
 Variables can swap their values using multiple assignment: a, b = b, a
 Augmented assignment operators like += are used as shortcuts.
+
+#------------------------------------------------------------------------------------------
 
 Methods are functions that are "called on" values.
 The index() list method returns the index of an item in the list.
@@ -23,6 +27,8 @@ The sort() list method sorts the items in a list.
 The sort() method's reverse=True keyword argument can make the sort() method sort in reverse order.
 These list methods operate on the list "in place", rather than returning a new list value.
 
+#------------------------------------------------------------------------------------------
+
 Strings can do a lot of the same things lists can do, but strings are immutable.
 Immutable values like strings and tuples cannot be modified "in place".
 Mutable values like lists can be modified in place.
@@ -31,8 +37,7 @@ When passing a list argument to a function, you are actually passing a list refe
 Changes made to a list in a function will affect the list outside the function.
 The \ line continuation character can be used to stretch Python instruction across multiple lines.
 
-
-
+#--------------------------------------------------------------------------------------------
 copy.copy vs copy.deepcopy
 >>> import copy
 >>> b = copy.copy(bacon)
