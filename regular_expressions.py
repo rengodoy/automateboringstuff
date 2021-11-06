22c05e9bc7f48f5c38061e9228b45085b9bd2fd8
Renato Machado de Godoy <renato.godoy@ifmg.edu.br>
2021-11-06 12:30:08 -0300

    lesson 26

--- regular_expressions.py
+++ regular_expressions.py
@@ -21,6 +21,22 @@ The curly braces can match a specific number of times.
 The curly braces with two numbers matches a minimum and maximum number of times.
 Leaving out the first or second number in the curly braces says there is no minimum or maximum.
 "Greedy matching" matches the longest string possible, "nongreedy matching" (or "lazy matching") matches the shortest string possible.
-Putting a question mark after the curly braces makes it do a nongreedy/lazy match.
+Putting a question mark (?) after the curly braces makes it do a nongreedy/lazy match.
+
+The regex method findall() is passed a string, and returns all matches in it, not just the first match.
+If the regex has 0 or 1 group, findall() returns a list of strings.
+If the regex has 2 or more groups, findall() returns a list of tuples of strings.
+\d is a shorthand character class that matches digits. \w matches "word characters" (letters, numbers, and the underscore). \s matches whitespace characters (space, tab, newline).
+The uppercase shorthand character classes \D, \W, and \S match charaters that are not digits, word characters, and whitespace.
+You can make your own character classes with square brackets: [aeiou]
+A ^ caret makes it a negative character class, matching anything not in the brackets: [^aeiou]
+
+\d Any numeric digit from 0 to 9
+\D Any character that is not a numeric digit from 0 to 9
+\w Any letter, numeric digit, or the underscore character. (Think of this as matching "word" characters.)
+\W Any character that is not a letter, numeric digit, or the underscore character.
+\s Any space, tab, os new line character. (Think of this as matching "space" characters)
+\S Any character that is not a space, tab or new line.
+
 
 """
