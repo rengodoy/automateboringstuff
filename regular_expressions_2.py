aca6fe99356fb2401c6695fea0e15d6720134e33
Renato Machado de Godoy <renato.godoy@ifmg.edu.br>
2021-11-07 12:50:10 -0300

    Exercicio pratico de Regex 2

--- regular_expressions_2.py
+++ regular_expressions_2.py
@@ -5,3 +5,23 @@ Write a function that uses regular expressions to make sure the password string 
 A strong password is defined as one that is at least eight characters long, contains both uppercase and lowercase characters, and has at least one digit.
 You may need to test the string against multiple regex patterns to validate its strength.
 '''
+import re
+
+def strongpasswd(password):
+    if len(password) >=  8 \
+    and re.search("^[a-zA-Z0-9]+", password) \
+    and re.search("[a-z]+", password) \
+    and re.search("[A-Z]+", password) \
+    and re.search("[0-9]+", password) \
+    and re.search("[@_!#$%^&*()<>?/\|}{~:]+", password):
+        print(password, True)
+        return True
+    else:
+        print(password, False)
+        return False
+
+strongpasswd('12345aF18@')
+strongpasswd('asdfgH1jk1')
+strongpasswd('asDf$##r4')
+strongpasswd('123$%Adf12')
+strongpasswd('asdadsfasd1')
