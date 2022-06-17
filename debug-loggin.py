2c2d20f86ab726a991da3a002171bfc956c451cb
Renato Machado de Godoy <renato.godoy@ifmg.edu.br>
2022-06-16 21:49:30 -0300

    Debug

--- debug-loggin.py
+++ debug-loggin.py
@@ -1,14 +1,17 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+#logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='debug-loogin.log', level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.disable(logging.DEBUG)
 
 logging.debug('Start of program')
 
 def factorial(n):
     logging.debug('Start of factorial(%s)' % (n))
     total = 1
-    for i in range(1, n + 1):
+    for i in range(1, n + 1):    
         total *= i
+        logging.info('i is ' + str(i) + ', total is ' + str(total))
         logging.debug('i is ' + str(i) + ', total is ' + str(total))
     logging.debug('End of factorial(%s)' % (n))
     return total
